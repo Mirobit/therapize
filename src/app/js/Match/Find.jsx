c78dc29b9ae34f7971618ee434c83e871d6664f7
Mirobit <13236924+Mirobit@users.noreply.github.com>
2018-09-27 20:50:15 +0200

    styling find

--- src/app/js/Match/Find.jsx
+++ src/app/js/Match/Find.jsx
@@ -6,7 +6,9 @@ import {
   Form,
   Input,
   Select,
+  Segment,
   TextArea,
+  Icon,
   Message
 } from "semantic-ui-react";
 
@@ -32,52 +34,65 @@ const optionsTopics = [
 
 const Find = ({ data, handleFindChange, find }) => {
   return (
-    <div className="container">
-      <Form>
-        Therapist preferences
-        <Form.Group widths="equal">
-          <Form.Field>
-            <label>Days</label>
-            <Dropdown
-              placeholder="Days"
-              name="days"
-              value={data.day}
+    <div className="flex-container-find">
+      <Segment style={{ width: "580px" }}>
+        <form className="ui form">
+          <span
+            style={{
+              color: "#666869",
+              fontSize: "23px",
+              display: "inline-block",
+              marginBottom: "20px"
+            }}
+          >
+            Preferences
+          </span>
+          <Form.Group>
+            <Form.Field width={5}>
+              <label>Days</label>
+              <Dropdown
+                placeholder="Days"
+                name="days"
+                value={data.day}
+                onChange={handleFindChange}
+                fluid
+                multiple
+                selection
+                options={optionsDays}
+                value={data.days}
+              />
+            </Form.Field>
+            <Form.Field width={5}>
+              <label>Topics</label>
+              <Dropdown
+                placeholder="Topics"
+                name="topics"
+                value={data.topics}
+                onChange={handleFindChange}
+                fluid
+                multiple
+                selection
+                options={optionsTopics}
+                value={data.topics}
+              />
+            </Form.Field>
+            <Form.Field
+              width={6}
+              control={Select}
+              label="Gender"
+              options={options}
+              placeholder="Gender"
+              name="gender"
+              value={data.gender}
               onChange={handleFindChange}
-              fluid
-              multiple
-              selection
-              options={optionsDays}
-              value={data.days}
             />
+          </Form.Group>
+          <Form.Field control={Button} color="teal" onClick={find}>
+            <Icon name="search" />
+            Find Therapist
           </Form.Field>
-          <Form.Field>
-            <label>Topics</label>
-            <Dropdown
-              placeholder="Topics"
-              name="topics"
-              value={data.topics}
-              onChange={handleFindChange}
-              fluid
-              multiple
-              selection
-              options={optionsTopics}
-              value={data.topics}
-            />
-          </Form.Field>
-          <Form.Field
-            control={Select}
-            label="Gender"
-            options={options}
-            placeholder="Gender"
-            name="gender"
-            value={data.gender}
-            onChange={handleFindChange}
-          />
-        </Form.Group>
-        <Form.Field control={Button} color="blue" onClick={find}>
-          Find Therapist
-        </Form.Field>
-      </Form>
+        </form>
+      </Segment>
     </div>
   );
 };
